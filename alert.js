e5bbc17fc1d175bdc5b884a3dae8a925aac08b48
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-06 17:30:24 +0600

    alert prompt confirm

---
+++ alert.js
@@ -0,0 +1,10 @@
+// alert something
+alert("this is an example of alert");
+
+// confirm something with to ok and cancel button. if user click cancel it's returns false and user click ok it's return true , on this case we can do any operation using condition or something
+confirm("raji?");
+
+const userInput = () => {
+  // use prompt for getting user input
+  prompt("What is your name?");
+};
