a132212b96dabb07665238533ad4a722b89e8bd8
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-06 22:20:25 +0600

    history api browser

---
+++ history.js
@@ -0,0 +1,8 @@
+// history.back returns the previous page
+history.back();
+// history.back returns the next page
+history.forward();
+// history.go returns back and forward page . -(back), +(forward)
+history.go(-1);
+// history.length count the total numbers of history
+history.length();
